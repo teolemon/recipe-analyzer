e8b4db8766cea91dd6710daef034941c01fb23f5
Kester Tong <kestert@google.com>
2020-01-19 11:43:36 -0500

    Add ingredient viewer and corresponding webpack and firebase hosting configuration

---
+++ index.ts
@@ -0,0 +1,12 @@
+import { FDCConfig } from './FDCConfig';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const ingredientName = document.getElementById('ingredient');
+  const fdcId = (new URL(document.location.href)).searchParams.get('fdcId');
+  const url = "https://api.nal.usda.gov/fdc/v1/" + encodeURIComponent(fdcId) + "?api_key=" + FDCConfig.apiKey;
+  fetch(url).then((response) => {
+    return response.json();
+  }).then((json) => {
+    return ingredientName.innerHTML = json.description;
+  });
+});
